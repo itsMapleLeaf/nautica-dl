ab5ac5c31dab29b8ca4eca38d55a1350e215cdf4
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-25 04:15:44 -0600

    add remix-typed

---
+++ app/remix-typed.ts
@@ -0,0 +1,56 @@
+import type { DataFunctionArgs } from "@remix-run/server-runtime"
+import { json, redirect, useActionData, useLoaderData } from "remix"
+
+type MaybePromise<Value> = Value | PromiseLike<Value>
+
+type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | { [Key in string]?: JsonValue }
+  | JsonValue[]
+
+export interface TypedResponse<Data extends JsonValue> extends Response {
+  json(): Promise<Data>
+}
+
+// prettier-ignore
+type LoaderFunctionTyped<Data extends JsonValue> = (args: DataFunctionArgs) =>
+  MaybePromise<Data | TypedResponse<Data>>
+
+type InferLoaderData<LoaderFunction> =
+  LoaderFunction extends LoaderFunctionTyped<infer Data> ? Data : never
+
+export function responseTyped(
+  body?: BodyInit | null,
+  init?: ResponseInit | number,
+) {
+  return new Response(
+    body,
+    typeof init === "number" ? { status: init } : init,
+  ) as TypedResponse<JsonValue>
+}
+
+export function jsonTyped<Data extends JsonValue>(
+  data: Data,
+  init?: ResponseInit | number,
+) {
+  return json(JSON.stringify(data), init) as TypedResponse<Data>
+}
+
+export function redirectTyped(url: string, init?: ResponseInit | number) {
+  return redirect(url, init) as TypedResponse<never>
+}
+
+export function useLoaderDataTyped<
+  LoaderFunction extends LoaderFunctionTyped<JsonValue>,
+>() {
+  return useLoaderData<InferLoaderData<LoaderFunction>>()
+}
+
+export function useActionDataTyped<
+  LoaderFunction extends LoaderFunctionTyped<JsonValue>,
+>() {
+  return useActionData<InferLoaderData<LoaderFunction>>()
+}
