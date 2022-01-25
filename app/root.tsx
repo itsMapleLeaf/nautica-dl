166fe0136db197a4722305292c5e5da9666932f7
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-25 02:51:38 -0600

    add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,23 +1,25 @@
+import type { MetaFunction } from "remix"
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from "remix"
+import tailwind from "./tailwind.out.css"
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+  return { title: "Nautica Downloader" }
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-stone-900 text-stone-100">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href={tailwind} />
         <Meta />
         <Links />
       </head>
@@ -28,5 +30,5 @@ export default function App() {
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
-  );
+  )
 }
